452449c817a952f205c527a9d475828e3d5e695a
Mubashir Yaseen <mubashiryaseen780@gmail.com>
2026-07-22 14:15:06 +0500

    backend compelete

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 import { Category, CategorySchema } from './schema/category.schema';
+import { UploadModule } from '../upload/upload.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }]),
+    UploadModule,
   ],
   controllers: [CategoryController],
   providers: [CategoryService],
